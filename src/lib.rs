6ed021579b6e8956a3dcb382bd6f14828b251a75
joaoviictorti <joaovictorti08@gmail.com>
2025-08-23 12:37:28 -0300

    style: collapse nested ifs flagged by clippy

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,13 @@
 #![no_std]
 #![doc = include_str!("../README.md")]
-#![allow(clippy::missing_transmute_annotations, clippy::useless_transmute)]
-#![allow(non_snake_case, non_camel_case_types, non_upper_case_globals)]
+#![allow(
+    clippy::missing_transmute_annotations, 
+    clippy::useless_transmute,
+    clippy::collapsible_if,
+    non_snake_case,
+    non_camel_case_types,
+    non_upper_case_globals
+)]
 
 extern crate alloc;
 
@@ -10,11 +16,8 @@ mod data;
 mod functions;
 mod gadget;
 mod stack;
-
-/// Responsible for memory obfuscation
 mod hypnus;
-pub use hypnus::*;
-
-/// Custom allocator for use in Heap Obfuscation
 mod allocator;
+
+pub use hypnus::*;
 pub use allocator::*;
